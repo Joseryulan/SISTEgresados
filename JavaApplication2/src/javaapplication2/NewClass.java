a7e6ef844d2d7dfe83b539036f768726bed1b659
sena <sena@sena-MS-7817>
2017-04-25 04:51:33 -0500

    hola mundo

---
+++ JavaApplication2/src/javaapplication2/NewClass.java
@@ -0,0 +1,59 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package javaapplication2;
+
+/**
+ *
+ * @author sena
+ */
+public class NewClass {
+   
+    
+    private int edad;
+    public void setEdad(int edadpara)
+    {
+    this.edad=edadpara;
+    }
+    public int getEdad()
+    {
+    return this.edad;
+    }
+    
+    private String nombre;
+    public void setNombre(String nombrepara)
+    {
+    this.nombre=nombrepara;
+    }
+    public  String getNombre()
+    {
+    return this.nombre;
+    }
+    
+    private int id;
+    public void setId(int idpara)
+    {
+    this.id=idpara;
+    }
+    public int getId()
+    {
+    return this.id;
+    }
+    
+    private String tipodocumento;
+    public void setTipodocumento(String tipodocumentopara)
+    {
+    this.tipodocumento=tipodocumentopara;
+    }
+    public  String getTipodocumento()
+    {
+    return this.tipodocumento;
+    }
+    
+}
+
+
+
+
